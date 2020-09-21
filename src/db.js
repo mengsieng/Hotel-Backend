0b2a168cc084aff7bc51a771a3f1f707bd1e1f39
mengsieng <mengsieng.him@trendsoftinnovation.com>
2020-09-21 11:52:05 +0700

    kk

--- src/db.js
+++ src/db.js
@@ -3,7 +3,8 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile');
 
-const enviroment = process.env.NODE_ENV || 'development';
+// const enviroment = process.env.NODE_ENV || 'development';
+const enviroment = 'development';
 const connectionConfig = knexConfig[enviroment];
 
 const connection = knex(connectionConfig);
