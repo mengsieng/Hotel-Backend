14779eb34579d477ed39447310a7232cade31454
mengsieng <mengsieng.him@trendsoftinnovation.com>
2020-09-28 16:48:27 +0700

    add null data

--- src/api/auth/auth.route.js
+++ src/api/auth/auth.route.js
@@ -32,7 +32,7 @@ router.post('/signUp', async (req, res, next) => {
         })
         const existUser = await User.query().findOne({ username }).first();
         if (existUser) {
-            res.json({ massage: 'This username already exisiting', statusCode: 2, });
+            res.json({ massage: 'This username already exisiting', statusCode: 2, data: null });
         } else {
             const hashPassword = await bcrypt.hash(password, 12);
             data = await User.query()
@@ -43,7 +43,9 @@ router.post('/signUp', async (req, res, next) => {
             };
             delete data.password;
             const token = await sign.sign(payload);
-            res.json({ token, data });
+            res.json({
+                token, data, message: 'success'
+            });
         }
     } catch (e) {
         next(e);
@@ -56,11 +58,11 @@ router.post('/signIn', async (req, res, next) => {
         const found = await User.query().findOne({ username }).first();
         console.log(found);
         if (found === undefined) {
-            res.status(200).json({ massage: 'Wrong username or password', statusCode: 2, });
+            res.status(200).json({ massage: 'Wrong username or password', statusCode: 2, data: null });
         }
         const validatePassword = await bcrypt.compare(password, found.password);
         if (!validatePassword) {
-            res.status(200).json({ massage: 'Wrong username or password', statusCode: 2, });
+            res.status(200).json({ massage: 'Wrong username or password', statusCode: 2, data: null });
         }
         const payload = {
             id: found.id,
@@ -68,7 +70,7 @@ router.post('/signIn', async (req, res, next) => {
         };
         delete found.password;
         const token = await sign.sign(payload);
-        res.json({ token, data: found });
+        res.json({ token, data: found, message: 'success' });
     } catch (e) {
         next(e);
     }
@@ -79,7 +81,7 @@ router.get('/getUserProfile', jwtChecker.checkToken, async (req, res, next) => {
     try {
         const data = await User.query().findById(id).first();
         delete data.password;
-        res.json({ data })
+        res.json({ data, message: 'success' })
     } catch (e) {
         next(e);
     }
@@ -91,7 +93,7 @@ router.patch('/update', jwtChecker.checkToken, async (req, res, next) => {
     try {
         const data = await User.query()
             .patchAndFetchById(id, { firstName, lastName, password, DOB, gender, phonenumber, username });
-        res.json({ data });
+        res.json({ data, message: 'success' });
     } catch (e) {
         next(e);
     }
@@ -104,7 +106,7 @@ router.patch('/changePassword', jwtChecker.checkToken, async (req, res, next) =>
         const found = await User.query().findById(id).first();
         const validatePassword = await bcrypt.compare(oldPassword, found.password);
         if (!validatePassword) {
-            res.status(200).json({ massage: 'Wrong password', statusCode: 2, });
+            res.status(200).json({ massage: 'Wrong password', statusCode: 2 });
         }
         const hashPassword = await bcrypt.hash(newPassword, 12);
         await User.query().patchAndFetchById(id, { password: hashPassword })
